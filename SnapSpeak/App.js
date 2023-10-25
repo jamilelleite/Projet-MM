d9defa4976449bb58871a89bb12b492dfb3059b6
eteuboudurel2002 <eteuboudurel@gmail.com>
2023-10-25 13:15:23 +0200

    Take picture view

--- SnapSpeak/App.js
+++ SnapSpeak/App.js
@@ -1,11 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
+import React, {useState, useEffect, useRef} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { CameraType, Camera  } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
+import Button  from './src/components/Button';
 
 export default function App() {
+  const [hasCameraPermission, setHascameraPermission] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const cameraRef = useRef(null);
+
+
+  useEffect(() =>{
+    (async () =>{
+      MediaLibrary.requestPermissionsAsync();
+      const cameraStatus = await Camera.requestCameraPermissionAsync();
+      setHascameraPermission(cameraStatus.status == 'granted');
+    })();
+  },[])
+
+
+
+ const takePicture = async () =>{
+  if(cameraRef){
+    try{
+      const data = await cameraRef.current.takePictureAsync();
+      console.log(data);
+      setImage(data.uri);
+
+    }catch(e){
+      console.log(e);
+    }
+  }
+ }
+
+if(hasCameraPermission === false ){
+  return <Text>No access to camera</Text>
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app SnapSpeak !</Text>
-      <StatusBar style="auto" />
+      <Camera 
+        style = {styles.camera}
+        type = {type}
+        FlashMode = {flash}
+        ref = {cameraRef}
+      >
+       
+      </Camera>
+      <View>
+        <Button title={'Take a picture'} icon="camera" onPress={takePicture}/>
+      </View>
     </View>
   );
 }
@@ -13,8 +59,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#000',
     justifyContent: 'center',
+    paddingBottom : 20,
   },
+
+  camera:{
+    flex: 1,
+    borderRadius : 20,
+  }
 });
